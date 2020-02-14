19c41bded43ec4b19d23792a6440c59a66ed099a
bj0rnps <55537500+bj0rnps@users.noreply.github.com>
2020-02-14 09:39:52 +0100

    Create get-smbstatus.ps1

---
+++ get-smbstatus.ps1
@@ -0,0 +1,35 @@
+
+#
+#
+#
+function get-smbstatus{
+
+
+begin{
+Add-Type -Language CSharp @"
+public class smb_1{
+public string SMB1Protocol ;
+public string SMB1Protocol_Client;
+public string SMB1Protocol_Server;
+public string SMB1Protocol_Deprecation;
+                  }
+"@
+}
+
+process{
+
+$obj=New-Object -TypeName smb_1
+$smb=Dism /online /Get-Features /format:table |where {$_ -like "*smb1P*"}
+                                                            
+$smb | foreach{
+                                                                          
+              $split=($_.split('|')).trim().replace('-','_') #remove whitespace replace char to match class prop
+              $obj.($split[0])=$split[1]
+              Write-Output $obj
+              }#end foreach smb
+                                                                          
+
+
+
+}
+}#end func
