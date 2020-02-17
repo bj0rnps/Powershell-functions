327f80977587b8f82ad27be81495c9be9fae27f2
bj0rnps <55537500+bj0rnps@users.noreply.github.com>
2020-02-17 19:59:42 +0100

    Update Get-Smb1status.ps1

--- Get-Smb1status.ps1
+++ Get-Smb1status.ps1
@@ -1,7 +1,17 @@
+<#
 
-#
-#
-#
+installstate posible values:
+Enabled (1)
+
+
+Disabled (2)
+
+
+Absent (3)
+
+
+Unknown (4)
+#>
 function Get-Smb1status{
 
 
@@ -16,16 +26,32 @@ public string SMB1Protocol_Deprecation;
 "@
 }
 
+
+
 process{
 
+
 $obj=New-Object -TypeName smb_1
-$smb=Dism /online /Get-Features /format:table |where {$_ -like "*smb1P*"}
-                                                            
+$smb=Get-WmiObject -query "select * from win32_optionalfeature where name like '%smb1%' "|select name,installstate
+
+
 $smb.foreach{
                                                                           
-              $split=($_.split('|')).trim().replace('-','_') #remove whitespace replace char to match class prop
-              $obj.($split[0])=$split[1]
-              
+           
+               $name=($_.name).replace('-','_').trim() 
+               $state=$_.installstate
+               
+               switch ($state)
+                            {
+                            1 {$state='Enabled'; Break}
+                            2 {$state="Disabled" ; Break }
+                            3 {$state="Absent" ; Break }
+                            4 {$state="Unknown" ; Break }
+                            
+                            }#end switch    
+          
+                 $obj.$name=$state
+                 
               }#end foreach smb
               Write-Output $obj                                                            
 
