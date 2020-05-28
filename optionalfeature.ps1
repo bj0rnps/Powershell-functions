0289a8d82c8e701c7c6fcb50618734beb6c3b70e
bj0rnps <55537500+bj0rnps@users.noreply.github.com>
2020-05-28 18:31:08 +0200

    now takes pipeline input
    Get-OptionalFeature | where {$_.name -like "*smb1*"}|Enable-OptionalFeature

--- optionalfeature.ps1
+++ optionalfeature.ps1
@@ -1,18 +1,43 @@
-function Enable-Optionalfeature {
+function Enable-OptionalFeature ($name){
 
-Get-WmiObject -query "select * from win32_optionalfeature "|select name,installstate|Out-GridView -OutputMode Multiple |%{
-$fname=$_.name
-Dism /Online /Enable-Feature /FeatureName:$fname
+ foreach($entry in $input){
+ $feature=$entry.name
+
+
+Dism /Online /Enable-Feature  /FeatureName:$feature /NoRestart #/quiet
+}
 
 }
-}
 
 
-function Disable-Optionalfeature {
-Get-WmiObject -query "select * from win32_optionalfeature "|select name,installstate|Out-GridView -OutputMode Multiple |%{
- $fname=$_.name
- Dism /Online /disable-Feature /FeatureName:$fname
-
- }             
+function Disable-OptionalFeature ([string]$name){
+ foreach($entry in $input){
+ $feature=$entry.name
+ Dism /Online /disable-Feature  /FeatureName:$feature /NoRestart #/quiet
+ }
+ 
  
  }                                         
+
+function Get-OptionalFeature {
+
+$win32_optionalfeature= Get-WmiObject -query "select * from win32_optionalfeature "|select name,installstate
+$win32_optionalfeature|%{
+               $state=$_.installstate
+               switch ($state)
+                            {
+                            1 {$state='Enabled'; Break}
+                            2 {$state="Disabled" ; Break }
+                            3 {$state="Absent" ; Break }
+                            4 {$state="Unknown" ; Break }
+                            
+                             }#end switch    
+
+$_.installstate=$state
+$win32_optionalfeature+=$_          
+} 
+ 
+$win32_optionalfeature|where {$_.installstate -ne [int]}
+ 
+ 
+ }                               
